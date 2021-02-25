79ab205cc17ccb12ce67270b01e3515d51f537bd
leejanghyun1 <63733215+leejanghyun1@users.noreply.github.com>
2021-02-25 18:15:34 +0900

    Imax 열린 모든 영화 제목들 크롤링

--- movie_crawler.py
+++ movie_crawler.py
@@ -4,10 +4,13 @@ from bs4 import BeautifulSoup
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20210228'
 html = requests.get(url)
 soup = BeautifulSoup(html.text, 'html.parser')
-imax = soup.select_one('span.imax')
-if(imax):
-    imax = imax.find_parent('div', class_ = 'col-times')
-    title = imax.select_one('div.info-movie > a > strong').text.strip()
-    print(title + " IMAX Open")
-else:
-    print("IMAX 예매가 열리지 않았습니다.")
+imax_list = soup.select('span.imax')
+
+# imax 열린 제목들 나열
+for imax in imax_list:
+    if(imax):
+        imax = imax.find_parent('div', class_='col-times')
+        title = imax.select_one('div.info-movie > a > strong').text.strip()
+        print(title + " IMAX Open")
+    else:
+        print("IMAX Not Open")
